1fe97249de7be5327ed3b0da84424c8faca37be8
Gaurav Kumar Yadav <gauravyadav00729@gmail.com>
2021-07-06 11:38:14 +0530

    done

--- js/main.js
+++ js/main.js
@@ -87,21 +87,15 @@ let winningFunc = () => {
       if (boxes[b[0]].id == "" || boxes[b[1]].id == "" || boxes[b[2]].id == "") {
           continue;
       } else if (boxes[b[0]].id == "X" && boxes[b[1]].id == "X" && boxes[b[2]].id == "X") {
-          // console.log("X is The Winner");
 
-          // Adding Winner text
           winnerName.innerText = `Player X Win The Game!`;
 
-          // show "Winner Page" & Hide "Mai Page"
           mainPage.style.display = "none";
           winner.style.display = "block";
       } else if (boxes[b[0]].id == "O" && boxes[b[1]].id == "O" && boxes[b[2]].id == "O") {
-          // console.log("O is The Winner");
 
-          // Adding Winner text
           winnerName.innerText = `Player O Win The Game!`;
 
-          // show "Winner Page" & Hide "Mai Page"
           mainPage.style.display = "none";
           winner.style.display = "block";
       } else {
@@ -111,22 +105,18 @@ let winningFunc = () => {
 }
 
 
-// Match Draw Function
 let drawFunc = () => {
   if (boxes[0].id != "" && boxes[1].id != "" &&
       boxes[2].id != "" && boxes[3].id != "" &&
       boxes[4].id != "" && boxes[5].id != "" &&
       boxes[6].id != "" && boxes[7].id != "" && boxes[8].id != "") {
-      // Adding "Draw" text
       winnerName.innerText = `Match Draw!`;
 
-      // show "Winner Page" & Hide "Mai Page"
       mainPage.style.display = "none";
       winner.style.display = "block";
   }
 }
 
-// Reset Game
 quit.addEventListener("click", () => {
   window.location.reload();
 })
